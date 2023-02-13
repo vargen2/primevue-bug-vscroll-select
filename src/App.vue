1025f36b0a0077b23aeffc390309db386b7c5a19
hjalmar <hjaarn2@gmail.com>
2023-02-13 15:52:31 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <h2>PrimeVue Issue Template</h2>
+    <p>Please create a test case and attach the link of the codesandbox to your github issue report.</p>
+    <Button type="button" label="PrimeVue Button"></Button>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  padding: 4rem;
+}
+</style>
